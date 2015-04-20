617ebe5df3ba035aa7b184f2cc4e741352da239b
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-04-20 15:05:18 -0700

    main html fixes

--- public/js/expenseTracker.js
+++ public/js/expenseTracker.js
@@ -1,18 +1,17 @@
 $(function () {
   var expenses = [];
+  var txnDate = fmMonth.value + fmDay.value + fmYear.value;
 
-  var Transaction = function(date, expense, cost, category){
+  var Transaction = function(date, expense, cost){
     this.date = date;
     this.expense = expense;
     this.cost = cost;
     this.category = category;
   }
 
-  expenses.push(new Transaction)
-
   $('#tableRender').click(function() {
-
-  })
+    expenses.push(new Transaction(txnDate, fmExpense.value, fmCost.value))
+  });
 
 
 })();
