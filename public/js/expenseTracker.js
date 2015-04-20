c9d4ef988cb6820943b3f487dd8e661506da37a7
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-04-20 14:05:12 -0700

    add constructor

--- public/js/expenseTracker.js
+++ public/js/expenseTracker.js
@@ -1,5 +1,18 @@
-(function () {
+$(function () {
+  var expenses = [];
 
+  var Transaction = function(date, expense, cost, category){
+    this.date = date;
+    this.expense = expense;
+    this.cost = cost;
+    this.category = category;
+  }
+
+  expenses.push(new Transaction)
+
+  $('#tableRender').click(function() {
+
+  })
 
 
 })();
