0d6b2c89562c7829e37fc8a468a4150d0ff28def
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-04-20 17:02:02 -0700

    figured out how to select input nodes with jQuery

--- public/js/expenseTracker.js
+++ public/js/expenseTracker.js
@@ -1,17 +1,28 @@
-$(function () {
+$(document).ready(function() {
   var expenses = [];
-  var txnDate = fmMonth.value + fmDay.value + fmYear.value;
 
+  //var txnDate = fmMonth.value + fmDay.value + fmYear.value;
   var Transaction = function(date, expense, cost){
     this.date = date;
     this.expense = expense;
     this.cost = cost;
     this.category = category;
-  }
+  };
 
   $('#tableRender').click(function() {
-    expenses.push(new Transaction(txnDate, fmExpense.value, fmCost.value))
-  });
+    var testInput = $("input[name='fmMonth']").val();
+    alert(testInput);
+  })
+});
 
 
-})();
+//
+//  $('#tableRender').click(function() {
+//    expenses.push(new Transaction(3, 2, 1));
+//    //expenses.push(new Transaction(txnDate, fmExpense.value, fmCost.value));
+//    console.log(expenses);
+//  })
+//}
+
+
+
