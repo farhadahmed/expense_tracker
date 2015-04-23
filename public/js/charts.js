bf9cd8eca465dd09b6489ee3d02a9e3ef0be3ec2
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-04-23 16:16:14 -0700

    Resolve merge conflicts for adding pie chart and line graph

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  //RENDER THE TABLE
   $('<tr><td>'+
     window.sessionStorage.getItem('txnMonth1')+'/'+
     window.sessionStorage.getItem('txnDay1')+'/'+
@@ -41,6 +42,237 @@ $(document).ready(function() {
       '<td>'+window.sessionStorage.getItem('txnExpense5')+'</td>'+
       '<td>$'+window.sessionStorage.getItem('txnCost5')+'</td></tr>').appendTo('#expenseTable');
   };
+
+
+
+  //LINE GRAPH
+  var userTxnData = {
+    labels : [
+    window.sessionStorage.getItem('txnMonth1')+'/'+window.sessionStorage.getItem('txnDay1')+'/'+window.sessionStorage.getItem('txnYear1'),
+    window.sessionStorage.getItem('txnMonth2')+'/'+window.sessionStorage.getItem('txnDay2')+'/'+window.sessionStorage.getItem('txnYear2'),
+    window.sessionStorage.getItem('txnMonth3')+'/'+window.sessionStorage.getItem('txnDay3')+'/'+window.sessionStorage.getItem('txnYear3'),
+    window.sessionStorage.getItem('txnMonth4')+'/'+window.sessionStorage.getItem('txnDay4')+'/'+window.sessionStorage.getItem('txnYear4'),
+    window.sessionStorage.getItem('txnMonth5')+'/'+window.sessionStorage.getItem('txnDay5')+'/'+window.sessionStorage.getItem('txnYear5')],
+    datasets : [
+
+    {
+
+      fillColor : "rgba(172,194,132,0.4)",
+      strokeColor : "#ACC26D",
+      pointColor : "#fff",
+      pointStrokeColor : "#9DB86D",
+      data : [
+      window.sessionStorage.getItem('txnCost1'),
+      window.sessionStorage.getItem('txnCost2'),
+      window.sessionStorage.getItem('txnCost3'),
+      window.sessionStorage.getItem('txnCost4'),
+      window.sessionStorage.getItem('txnCost4')]
+    }
+    ]
+    }
+  var user = document.getElementById('line-graph').getContext('2d');
+  new Chart(user).Line(userTxnData);
+
+
+
+  //PIE CHART
+  (function() {
+
+    //Grab category arrays from session and assign to variable
+    var txnIncome = window.sessionStorage.getItem('incomeAmt');
+    var tempHousing = window.sessionStorage.getItem('sessionCatHousing');
+    var tempAuto = window.sessionStorage.getItem('sessionCatAuto');
+    var tempInsurance = window.sessionStorage.getItem('sessionCatInsurance');
+    var tempDebts = window.sessionStorage.getItem('sessionCatDebts');
+    var tempEntertainment = window.sessionStorage.getItem('sessionCatEntertainment');
+    var tempClothing = window.sessionStorage.getItem('sessionCatClothing');
+    var tempSavings = window.sessionStorage.getItem('sessionCatSavings');
+    var tempMedical = window.sessionStorage.getItem('sessionCatMedical');
+    var tempMisc = window.sessionStorage.getItem('sessionCatMisc');
+
+    //Convert stringified arrays back into int arrays
+    //Housing
+    var catHousing = new Array();
+    catHousing = tempHousing.split(','); //catAuto is now an array of string ints
+    for (i in catHousing) {
+      catHousing[i] = parseInt(catHousing[i]); //catAuto is now an array of ints
+    }
+    //Auto
+    var catAuto = new Array();
+    catAuto = tempAuto.split(','); //catAuto is now an array of string ints
+    for (i in catAuto) {
+      catAuto[i] = parseInt(catAuto[i]); //catAuto is now an array of ints
+    }
+    //Insurance
+    var catInsurance = new Array();
+    catInsurance = tempInsurance.split(','); //catInsurance is now an array of string ints
+    for (i in catInsurance) {
+      catInsurance[i] = parseInt(catInsurance[i]); //catInsurance is now an array of ints
+    }
+    //Debts
+    var catDebts = new Array();
+    catDebts = tempDebts.split(','); //catDebts is now an array of string ints
+    for (i in catDebts) {
+      catDebts[i] = parseInt(catDebts[i]); //catDebts is now an array of ints
+    }
+    //Entertainment
+    var catEntertainment = new Array();
+    catEntertainment = tempEntertainment.split(','); //catEntertainment is now an array of string ints
+    for (i in catEntertainment) {
+      catEntertainment[i] = parseInt(catEntertainment[i]); //catEntertainment is now an array of ints
+    }
+    //Clothing
+    var catClothing = new Array();
+    catClothing = tempClothing.split(','); //catClothing is now an array of string ints
+    for (i in catClothing) {
+      catClothing[i] = parseInt(catClothing[i]); //catClothing is now an array of ints
+    }
+    //Savings
+    var catSavings = new Array();
+    catSavings = tempSavings.split(','); //catSavings is now an array of string ints
+    for (i in catSavings) {
+      catSavings[i] = parseInt(catSavings[i]); //catSavings is now an array of ints
+    }
+    //Medical
+    var catMedical = new Array();
+    catMedical = tempMedical.split(','); //catMedical is now an array of string ints
+    for (i in catMedical) {
+      catMedical[i] = parseInt(catMedical[i]); //catMedical is now an array of ints
+    }
+    //Misc
+    var catMisc = new Array();
+    catMisc = tempMisc.split(','); //catMisc is now an array of string ints
+    for (i in catMisc) {
+      catMisc[i] = parseInt(catMisc[i]); //catMisc is now an array of ints
+    }
+
+
+    //Get sum of arrays
+    //Housing
+    var housingTotal = 0;
+    $.each(catHousing, function() {
+      if (!isNaN(this)) {
+        housingTotal += this;
+      };
+    });
+    //Auto
+    var autoTotal = 0;
+    $.each(catAuto, function() {
+      if (!isNaN(this)) {
+        autoTotal += this;
+      };
+    });
+    //Insurance
+    var insuranceTotal = 0;
+    $.each(catInsurance, function() {
+      if (!isNaN(this)) {
+        insuranceTotal += this;
+      };
+    })
+    //Debts
+    var debtsTotal = 0;
+    $.each(catDebts, function() {
+      if (!isNaN(this)) {
+        debtsTotal += this;
+      }
+    })
+    //Entertainment
+    var entertainmentTotal = 0;
+    $.each(catEntertainment, function() {
+      if (!isNaN(this)) {
+        entertainmentTotal += this;
+      };
+    })
+    //Clothing
+    var clothingTotal = 0;
+    $.each(catClothing, function() {
+      if (!isNaN(this)) {
+        clothingTotal += this;
+      };
+    })
+    //Savings
+    var savingsTotal = 0;
+    $.each(catSavings, function() {
+      if (!isNaN(this)) {
+        savingsTotal += this;
+      };
+    });
+    //Medical
+    var medicalTotal = 0;
+    $.each(catMedical, function() {
+      if (!isNaN(this)) {
+        medicalTotal += this;
+      };
+    });
+    //Misc
+    var miscTotal = 0;
+    $.each(catMisc, function() {
+      if (!isNaN(this)) {
+        miscTotal += this;
+      };
+    })
+    console.log('catAuto ' + catAuto);
+    console.log('autoTotal ' + autoTotal);
+    console.log('catHousing ' + catHousing);
+    console.log('housingTotal ' + housingTotal);
+    console.log(debtsTotal);
+
+
+    //Create pie chart
+    var pieData = [
+    {
+      value: (100/(txnIncome/housingTotal)),
+      color: '#ADD6FF',
+      label: 'Housing'
+    },
+    {
+      value: (100/(txnIncome/autoTotal)),
+      color: '#70B8FF',
+      label: 'Auto'
+    },
+    {
+      value: (100/(txnIncome/insuranceTotal)),
+      color: '#99CCFF',
+      label: 'Insurance'
+    },
+    {
+      value: (100/(txnIncome/debtsTotal)),
+      color: '#297ACC',
+      label: 'Debts'
+    },
+    {
+      value: (100/(txnIncome/entertainmentTotal)),
+      color: '#1F5C99',
+      label: 'Entertainment'
+    },
+    {
+      value: (100/(txnIncome/clothingTotal)),
+      color: '#143D66',
+      label: 'Clothing'
+    },
+    {
+      value: (100/(txnIncome/savingsTotal)),
+      color: '#3399FF',
+      label: 'Savings'
+    },
+    {
+      value: (100/(txnIncome/medicalTotal)),
+      color: '#85C2FF',
+      label: 'Medical/Dental'
+    },
+    {
+      value: (100/(txnIncome/miscTotal)),
+      color: '#5CADFF',
+      label: 'Miscellaneous'
+    }
+    ];
+    var pieOptions = {
+      segmentShowStroke: false,
+      animateScale: true
+    };
+    var countries = document.getElementById('pie-chart').getContext('2d');
+    new Chart(countries).Pie(pieData, pieOptions);
+  })();//end pie chart iife
 
 
 });
--- public/js/expenseTracker.js
+++ public/js/expenseTracker.js
@@ -1,46 +1,99 @@
 $(document).ready(function() {
-  var expenses = [];
+  var expenses;
   var i = 0; //index created for adding user input to session storage
-  var txnMonth, txnDay, txnYear, txnDate;
+  var txnMonth, txnDay, txnYear, txnDate, txnCategory;
+  var catHousing = [];
+  var catAuto = [];
+  var catInsurance = [];
+  var catDebts = [];
+  var catEntertainment = [];
+  var catClothing = [];
+  var catSavings = [];
+  var catMedical = [];
+  var catMisc = [];
 
   $('#expenseForm').submit(function(e) { //override form default so it
     return false;                        //doesn't refresh with submit.
   })
 
-  var Transaction = function(date, expense, cost){
+  var Transaction = function(date, expense, category, cost){
     this.date = date;
     this.expense = expense;
+    this.category = category;
     this.cost = cost;
   };
 
   $('#tableRender').click(function() {
     i++;
-    expenses = [];
+    expenses = []; //array of objects
+
+    //Get input values
+    txnIncome = $("input[name='fmIncome']").val();
     txnMonth = $("select[name='fmMonth']").val();
     txnDay = $("select[name='fmDay']").val();
     txnYear = $("select[name='fmYear']").val();
     txnDate = txnMonth + txnDay + txnYear;
-
     txnExpense = $("input[name='fmExpense']").val();
     txnCost = $("input[name='fmCost']").val();
+    txnCategory = $("select[name='fmCategory']").val();
 
-    expenses.push(new Transaction(txnDate, txnExpense, txnCost));
+    //Push input values into an array of objects
+    expenses.push(new Transaction(txnDate, txnExpense, txnCategory, txnCost));
+
+    //Push cost value to correct category array
+    switch(txnCategory) {
+      case 'Auto':
+        catAuto.push(txnCost);
+        break;
+      case 'Housing':
+        catHousing.push(txnCost);
+        break;
+      case 'Insurance':
+        catInsurance.push(txnCost);
+        break;
+      case 'Debts':
+        catDebts.push(txnCost);
+        break;
+      case 'Entertainment':
+        catEntertainment.push(txnCost);
+        break;
+      case 'Clothing':
+        catClothing.push(txnCost);
+        break;
+      case 'Savings':
+        catSavings.push(txnCost);
+        break;
+      case 'Medical/Dental':
+        catMedical.push(txnCost);
+        break;
+      case 'Miscellaneous':
+        catMisc.push(txnCost);
+        break;
+    }
+
+    //Render new row in table with buttonClick
     $('<tr><td>'+txnMonth+'/'+txnDay+'/'+txnYear+'</td>' + '<td>'+txnExpense+'</td>' + '<td>$'+txnCost+'</td></tr>').appendTo('#expenseTable');
 
     if (window.sessionStorage) {
-      //store info in session storage
-      sessionStorage.setItem('txnMonth'+i, $("select[name='fmMonth']").val());
-      sessionStorage.setItem('txnDay'+i, $("select[name='fmDay']").val());
-      sessionStorage.setItem('txnYear'+i, $("select[name='fmYear']").val());
-      sessionStorage.setItem('txnExpense'+i, $("input[name='fmExpense']").val());
-      sessionStorage.setItem('txnCost'+i, $("input[name='fmCost']").val());
+      //store input data in session storage
+      sessionStorage.setItem('incomeAmt', txnIncome); //Since this doesn't change, no '+i' needed
+      sessionStorage.setItem('txnMonth'+i, txnMonth);
+      sessionStorage.setItem('txnDay'+i, txnDay);
+      sessionStorage.setItem('txnYear'+i, txnYear);
+      sessionStorage.setItem('txnExpense'+i, txnExpense);
+      sessionStorage.setItem('txnCategory'+i, txnCategory);
+      sessionStorage.setItem('txnCost'+i, txnCost);
 
-      //Output stored data
-      alert('txnMonth'+i+' = ' + window.sessionStorage.getItem('txnMonth'+[i]));
-      alert('txnDay'+i+' = ' + window.sessionStorage.getItem('txnDay'+[i]));
-      alert('txnYear'+i+' = ' + window.sessionStorage.getItem('txnYear'+[i]));
-      alert('txnExpense'+i+' = ' + window.sessionStorage.getItem('txnExpense'+[i]));
-      alert('txnCost'+i+' = ' + window.sessionStorage.getItem('txnCost'+[i]));
+      //store arrays of categories in session storage
+      sessionStorage.setItem('sessionCatAuto', catAuto);
+      sessionStorage.setItem('sessionCatHousing', catHousing);
+      sessionStorage.setItem('sessionCatInsurance', catInsurance);
+      sessionStorage.setItem('sessionCatDebts', catDebts);
+      sessionStorage.setItem('sessionCatEntertainment', catEntertainment);
+      sessionStorage.setItem('sessionCatClothing', catClothing);
+      sessionStorage.setItem('sessionCatSavings', catSavings);
+      sessionStorage.setItem('sessionCatMedical', catMedical);
+      sessionStorage.setItem('sessionCatMisc', catMisc);
 
     } else {
       alert('Sorry. Your browser does not support session storage.');
