d0706f8c5b82d55b6de0e92d596f4a7041f5ebb0
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-04-23 19:45:48 -0700

    Merge of Mateja's aboutUs

---
+++ public/js/aboutUs.js
@@ -0,0 +1,25 @@
+var $main = $('#main'),
+$articles = $main.children('article'), timeout;
+$articles.on('mouseenter', function (event) {
+  var $article = $(this);
+  clearTimeout (timeout);
+  timeout = setTimeout(function() {
+
+      if ($article.hasClass('active')) return false;
+
+      $articles.not ($article.removeClass('blur').addClass('active'))
+        .removeClass('active')
+        .addClass('blur');
+
+      }, 65);
+
+
+});
+
+$main.on('mouseleave', function(event) {
+    clearTimeout(timeout);
+    $articles.removeClass('active blur');
+
+});
+/*portions of this code were modified and adapted from*/
+/*http://tympanus.net/codrops/2011/12/14/item-blur-effect-with-css3-and-jquery/*/
